637780f211b865147cd05252da4c0b2d3a6babcd
kcode_tech <kos.ter.code@gmail.com>
2025-12-15 10:22:39 +0200

    updated sqlx && tx errors

--- kafka-outbox/internal/repos/payment.repo.go
+++ kafka-outbox/internal/repos/payment.repo.go
@@ -42,24 +42,37 @@ func NewPaymentRepo(db *sqlx.DB) *PaymentRepo {
 }
 
 func (r *PaymentRepo) Insert(ctx context.Context, tx *sqlx.Tx, p *Payment) (int, error) {
-	query := fmt.Sprintf("INSERT INTO %s (order_number, amount, status, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING id", r.tableName)
-
-	rows, err := tx.QueryxContext(ctx, query, p.OrderNumber, p.Amount, p.Status, p.CreatedAt, p.UpdatedAt)
+	// v1 ->pretties, but overhead of prepare
+	query := fmt.Sprintf("INSERT INTO %s (order_number, amount, status, created_at, updated_at) VALUES(:order_number, :amount, :status, :created_at, :updated_at) RETURNING id", r.tableName)
+	stmt, err := tx.PrepareNamedContext(ctx, query)
 	if err != nil {
-		fmt.Printf("err on insert = %v\n", err)
 		return dbpostgres.NonExistingIntKey, err
 	}
-	defer rows.Close()
-	var id int
-	for rows.Next() {
-		ids, err := rows.SliceScan()
-		if err != nil {
-			fmt.Printf("unable to get last insetID = %v\n", err)
-			return dbpostgres.NonExistingIntKey, err
-		}
-		id = int(ids[0].(int64))
+	defer stmt.Close()
+	err = stmt.Get(p, p)
+	if err != nil {
+		return dbpostgres.NonExistingIntKey, err
 	}
-	return id, nil
+	return p.ID, nil
+
+	// v1 -> no prepare, better perf-ce
+	// query := fmt.Sprintf("INSERT INTO %s (order_number, amount, status, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING id", r.tableName)
+	// rows, err := tx.QueryxContext(ctx, query, p.OrderNumber, p.Amount, p.Status, p.CreatedAt, p.UpdatedAt)
+	// if err != nil {
+	// 	fmt.Printf("err on insert = %v\n", err)
+	// 	return dbpostgres.NonExistingIntKey, err
+	// }
+	// defer rows.Close()
+	// var id int
+	// for rows.Next() {
+	// 	ids, err := rows.SliceScan()
+	// 	if err != nil {
+	// 		fmt.Printf("unable to get last insetID = %v\n", err)
+	// 		return dbpostgres.NonExistingIntKey, err
+	// 	}
+	// 	id = int(ids[0].(int64))
+	// }
+	// return id, nil
 }
 
 func (r *PaymentRepo) Get(ctx context.Context, tx *sqlx.Tx, ID int) *Payment {
