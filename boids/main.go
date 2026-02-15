22bb544832ef2bdceef9ce4507322776816cf657
kcode_tech <kos.ter.code@gmail.com>
2026-02-15 10:36:39 +0200

    added gif bg

--- boids/main.go
+++ boids/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/draw"
+	"image/gif"
 	"log"
 	"math/rand"
 	"os"
@@ -12,11 +15,11 @@ import (
 )
 
 const (
-	screenHeight   = 640.00 * 2
-	screenWidth    = 1280.00 * 2
-	boidsCount     = 300
+	screenHeight   = 640 * 2
+	screenWidth    = 1080.00 * 2
+	boidsCount     = 600
 	boidSize       = 7
-	fishTargetSize = 75
+	fishTargetSize = 50
 
 	alignRadius = fishTargetSize * 1.5
 	alightForce = 0.05
@@ -32,6 +35,8 @@ const (
 
 	wallSepDistance = screenWidth / 15
 	wallSepForce    = 0.75
+
+	bgPath = "./assets/bg.gif"
 )
 
 var (
@@ -39,11 +44,19 @@ var (
 )
 
 type Game struct {
-	boids []*Boid
+	boids    []*Boid
+	bgFrames []*ebiten.Image
+	bgDelays []int // delay per frame in 100ths of a second
+	bgFrame  int   // current frame index
+	bgTick   int   // counts up each Update()
 }
 
 func NewGame() *Game {
 	g := &Game{}
+	bgs, dels := loadGIF(bgPath)
+
+	g.bgDelays = dels
+	g.bgFrames = bgs
 
 	boids := make([]*Boid, boidsCount)
 	for id := range boidsCount {
@@ -58,6 +71,19 @@ func NewGame() *Game {
 }
 
 func (g *Game) Update() error {
+	g.bgTick++
+
+	throshold := g.bgDelays[g.bgFrame] * 60 / 100
+	if throshold < 1 {
+		throshold = 1
+	}
+
+	if g.bgTick >= throshold {
+		g.bgTick = 0
+		lbg := len(g.bgFrames)
+		g.bgFrame = (g.bgFrame + 1) % lbg
+	}
+
 	for _, b := range g.boids {
 		b.Update(g)
 	}
@@ -65,7 +91,13 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// screen.Fill(color.RGBA{255, 245, 228, 0xff})
+
+	op := &ebiten.DrawImageOptions{}
+	bg := g.bgFrames[g.bgFrame]
+	scaleX := screenWidth / float64(bg.Bounds().Dx())
+	scaleY := screenHeight / float64(bg.Bounds().Dy())
+	op.GeoM.Scale(scaleX, scaleY)
+	screen.DrawImage(bg, op)
 
 	for _, b := range g.boids {
 		b.Draw(screen)
@@ -76,6 +108,33 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 func (g *Game) Layout(_, _ int) (sw, sh int) {
 	return screenWidth, screenHeight
+}
+
+func loadGIF(path string) ([]*ebiten.Image, []int) {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	g, err := gif.DecodeAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	frames := make([]*ebiten.Image, len(g.Image))
+	delays := make([]int, len(g.Delay))
+
+	bounds := g.Image[0].Bounds()
+	rgba := image.NewRGBA(bounds)
+
+	for i, frame := range g.Image {
+		draw.Draw(rgba, bounds, frame, bounds.Min, draw.Over)
+		frames[i] = ebiten.NewImageFromImage(rgba)
+		delays[i] = g.Delay[i]
+	}
+
+	return frames, delays
 }
 
 func init() {
